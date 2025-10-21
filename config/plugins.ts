7bf89fe3ba438cf797e2c5f9e9a8c619915a302e
José Montoya <125832010+JrMontoya490@users.noreply.github.com>
2025-10-21 10:50:29 -0600

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env('dv3oumr8f'),
+        api_key: env('429984587841566'),
+        api_secret: env('KXehwEf-amL6INd5XqS6ubQNDKY'),
       },
       actionOptions: {
         upload: {},
