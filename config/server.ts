861fe6dc9b0bda869b0e7e33e0e17d4bd79302e8
José Montoya <125832010+JrMontoya490@users.noreply.github.com>
2025-10-21 12:07:57 -0600

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,15 +1,25 @@
+// Path: ./config/server.ts
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
+  
+  // 💡 ¡Añade esta propiedad! Esto le dice a Koa que confíe en el proxy de Render.
+  proxy: {
+    koa: true,
+  },
+  
   admin: {
     auth: {
       secret: env('ADMIN_AUTH_SECRET'),
     },
-    url: '/',
+    // Asegúrate de que esta URL use la variable de entorno (la tienes configurada correctamente en Render).
+    url: env('RENDER_EXTERNAL_URL', '/'),
     serveAdminPanel: true,
+    // La variable STRAPI_ADMIN_FORCE_HTTP que tienes en true ('ttue' en la imagen) es correcta para forzar el protocolo.
     forceHttps: env.bool('STRAPI_ADMIN_FORCE_HTTP', false),
   },
 });
