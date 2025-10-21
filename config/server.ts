01b2f1ca1a20d34e04aed5ca13592296064adcb9
José Montoya <125832010+JrMontoya490@users.noreply.github.com>
2025-10-21 11:52:40 -0600

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,9 +1,15 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('RENDER_EXTERNAL_URL', 'https://backend-ena.onrender.com'),
-  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_AUTH_SECRET'),
+    },
+    url: '/',
+    serveAdminPanel: true,
+    forceHttps: env.bool('STRAPI_ADMIN_FORCE_HTTP', false),
   },
 });
