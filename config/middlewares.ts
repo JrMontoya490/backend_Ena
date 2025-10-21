24328102242612343f6914b5b39fa0c8c913491c
José Montoya <125832010+JrMontoya490@users.noreply.github.com>
2025-10-21 12:58:30 -0600

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,32 @@
+// Path: ./config/middlewares.ts
+
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
