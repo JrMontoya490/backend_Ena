162c496a362260b3a5e0bd05fe82e9afa4d699ad
José Montoya <125832010+JrMontoya490@users.noreply.github.com>
2025-10-21 15:51:24 -0600

    Create database.ts

---
+++ config/database.ts
@@ -0,0 +1,12 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+      },
+    },
+    useNullAsDefault: true,
+  },
+});
